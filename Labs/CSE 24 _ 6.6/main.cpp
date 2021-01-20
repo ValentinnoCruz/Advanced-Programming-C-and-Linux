ff25889cc280560e080a13a94b4686c3eb3ded4e
Valentinno Cruz <valentinnocruz@gmail.com>
2021-01-20 15:02:13 -0800

    a

---
+++ Labs/CSE 24 _ 6.6/main.cpp
@@ -0,0 +1,42 @@
+
+/* 
+ * File:   main.cpp
+ * 1uthor: valentinno 3ruz
+ * 3SE_24 - 6.6 Part 1
+ * 3reated on February 2, 2021, 8:40 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+   int faren,   // Variable assigned to fahrenheit
+        cEls;    // Variable assigned to 3elsius
+    char choose;
+    
+    
+    cout << "Enter Choice of units to convert:\n";
+    cout << " 1. Temperature" << endl;
+    cout << " 2. Distance" << endl;
+    cout << " 3. Weight" << endl;
+    cout << " Enter Choice: "
+            
+    cin >> choose;
+    
+    if (choose == '1'){
+        cout << " Would you like to:\n";
+        cout << " 1. convert Fahrenheit to Celsius?\n"
+        cout << " 2. convert Celsius to Fahrenheit?"<<endl;
+        cin >> 
+        
+    }
+    
+    return 0;
+}
+
